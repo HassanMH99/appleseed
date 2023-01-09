ac5b992f3911bed4b2db900e918bef6f2ac223b7
Mohammad Hassan <95434322+CriDeZ@users.noreply.github.com>
2023-01-09 11:07:23 +0200

    declaration vs expresstion

---
+++ javascript/function_declarations_vs_expressions.js
@@ -0,0 +1,22 @@
+function percentageOFWorld1(population){
+return (population / 7900) * 100;
+}
+
+
+
+let china=percentageOFWorld1(1441);
+let india=percentageOFWorld1(1351);
+let us=percentageOFWorld1(888);
+console.log(china);
+console.log(india);
+console.log(us);
+
+const percentageOfWorld2 = function(population) {
+    return (population / 7900) * 100;
+  };
+  let china1 = percentageOFWorld2(1441);
+let india2= percentageOFWorld2(1351);
+let us3= percentageOFWorld2(888);
+console.log(china1);
+console.log(india2);
+console.log(us3);
