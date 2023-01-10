b92b386732d85fc15fb0133084eeab1b564e9bdc
Mohammad Hassan <95434322+CriDeZ@users.noreply.github.com>
2023-01-10 10:44:24 +0200

    About Language

---
+++ javascript/langauges.js
@@ -0,0 +1,25 @@
+function whatlanguage(language){
+    switch(language){
+        case "mandarin":
+            console.log('MOST number of native speakers!');
+        break;
+        case "Spanish":
+            console.log('2nd place in a number of native speakers');  
+        break;
+        case "English":
+            console.log('3rd place');  
+        break;
+        case "Hindi":
+            console.log('Number 4');  
+        break;
+        case "Arabic":
+            console.log('5th most spoken language');  
+        break;
+        default:console.log('Not in the top 5');
+    }
+
+}
+
+whatlanguage('mandarin');
+whatlanguage('Arabic');
+whatlanguage('Hebrew');
