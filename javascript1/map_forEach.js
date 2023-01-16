aa56cde3b0e7eaeade70f9a4db14c7e5ec748a38
Mohammad Hassan <95434322+CriDeZ@users.noreply.github.com>
2023-01-16 11:30:21 +0200

    foreach_map

---
+++ javascript1/map_forEach.js
@@ -0,0 +1,53 @@
+const newReleases = [
+  {
+    id: 70111470,
+    title: "Die Hard",
+    boxart: "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+    uri: "http://api.netflix.com/catalog/titles/movies/70111470",
+    rating: [4.0],
+    bookmark: [],
+  },
+  {
+    id: 654356453,
+    title: "Bad Boys",
+    boxart: "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+    uri: "http://api.netflix.com/catalog/titles/movies/70111470",
+    rating: [5.0],
+    bookmark: [{ id: 432534, time: 65876586 }],
+  },
+  {
+    id: 65432445,
+    title: "The Chamber",
+    boxart: "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
+    uri: "http://api.netflix.com/catalog/titles/movies/70111470",
+    rating: [4.0],
+    bookmark: [],
+  },
+  {
+    id: 675465,
+    title: "Fracture",
+    boxart: "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
+    uri: "http://api.netflix.com/catalog/titles/movies/70111470",
+    rating: [5.0],
+    bookmark: [{ id: 432534, time: 65876586 }],
+  },
+];
+
+function getIdAndTitle(movies){
+    let result = [];
+    movies.forEach((movie) => {
+        result.push({id:movie.id,title:movie.title});
+
+    });
+    return result;
+}
+console.log(getIdAndTitle(newReleases));
+
+function getIdAndTitle2(movies){
+   return movies.map(function(movie){
+    return {id:movie.id,title:movie.title}
+   });
+
+   
+}
+console.log(getIdAndTitle2(newReleases));
