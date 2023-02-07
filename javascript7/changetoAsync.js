ada74acce9f4a638d4f619e4e0b01fa9c3d7e4b5
Mohammad Hassan <95434322+CriDeZ@users.noreply.github.com>
2023-02-07 12:17:19 +0200

    Change it to async

---
+++ javascript7/changetoAsync.js
@@ -0,0 +1,47 @@
+const getIDs = () =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve([532, 543, 753, 1, 5]);
+    }, 1500);
+  });
+const getRecipe = (recipeID) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(
+      (ID) => {
+        const recipe = {
+          title: "Fresh tomato pasta",
+          publisher: "Pinchas Hodadad",
+        };
+        resolve(`${ID}: ${recipe.title}`);
+      },
+      1500,
+      recipeID
+    );
+  });
+};
+// getIDs()
+//   .then((IDs) => {
+//     console.log(IDs);
+//     return getRecipe(IDs[2]);
+//   })
+//   .then((recipe) => {
+//     console.log(recipe);
+//   })
+//   .catch((error) => {
+//     console.log("It is an error!");
+//   });
+
+// change i to async
+const getdata = async ()=>{
+    try{
+        const id = await getIDs();
+        console.log(id);
+        const recipe = await getRecipe(id[2]);
+        console.log(recipe);
+
+    }
+    catch(error){
+        console.log("there is an error");
+    }
+}
+getdata();
